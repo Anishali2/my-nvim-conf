1f858b0295944b02c052172142932f5a1bed0c2f
Anishali2 <ansihali1221@gmail.com>
2025-01-13 16:32:08 +0500

    which key issue fix

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -6,17 +6,8 @@ return {
     local wk = require("which-key")
     wk.setup({
       keys = {
-        -- Use `keys` instead of `popup_mappings`
         scroll_down = '<PageDown>',
         scroll_up = '<PageUp>',
-      },
-      defer = {
-        gc = "Comments", -- Use `defer` instead of `operators`
-      },
-      icons = {
-        breadcrumb = "»",
-        separator = "➜",
-        group = "+",
       },
     })
   end,
